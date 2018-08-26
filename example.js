857bf916d1310e68fa40226fe8d784c60872b628
Terkel <terkel@terkel.com>
2018-08-26 10:04:43 +0200

    v1.0.0 (#80)

--- example.js
+++ example.js
@@ -8,23 +8,19 @@ const { prompt } = require('./lib');
             type: 'text',
             name: 'twitter',
             message: `What's your twitter handle?`,
-            initial: 'terkelg',
+            initial: `terkelg`,
             format: v => `@${v}`
+        },
+        {
+            type: 'number',
+            name: 'age',
+            message: 'How old are you?',
+            validate: value => value < 18 ? `Sorry, you have to be 18` : true
         },
         {
             type: 'password',
             name: 'secret',
             message: 'Tell me a secret'
-        },
-        {
-            type: 'invisible',
-            name: 'password',
-            message: `Enter password`
-        },
-        {
-            type: 'number',
-            name: 'age',
-            message: 'How old are you?'
         },
         {
             type: 'confirm',
